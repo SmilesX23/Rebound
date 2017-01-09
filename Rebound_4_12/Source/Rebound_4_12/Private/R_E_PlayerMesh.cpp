a804904b8711f921295e483d8abac02466ec85f1
Kieran Gallagher <kags1995@hotmail.co.uk>
2017-01-09 20:30:15 +0000

    Revert "Merge remote-tracking branch 'refs/remotes/origin/Art'"
    This reverts commit 434e144d35f15de0b2de79069aa2785325afdb7f, reversing
    changes made to e10e1e4d5d64cf8c272cb7e09b0276b6ac15465e.

--- Rebound_4_12/Source/Rebound_4_12/Private/R_E_PlayerMesh.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Rebound_4_12.h"
-#include "R_E_PlayerMesh.h"
-
-
-
-
--- Rebound_4_12/Source/Rebound_4_12/Private/R_S_LevelStruct.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Rebound_4_12.h"
-#include "R_S_LevelStruct.h"
-
-
-
-
